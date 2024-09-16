dfc5496074ce50cda25b72b8e057d12e4ab5a05e
ushna parvaiz <ushnapervaiz10@gmail.com>
2024-09-16 09:27:48 +0500

    change in file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Greets from './components/Greets'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Greets name ='ushnaa'/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/Greets.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// function Greet(){
+// return <h1> this is first line</h1>
+
+// }
+//arrow function
+const Greets=(props)=> { return <h1> hello{ props.name}</h1>}
+export default Greets;
